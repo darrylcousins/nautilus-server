843acb07dfd1504a21c0392c35d47742db7a6131
Darryl Cousins <darryljcousins@gmail.com>
2019-01-06 11:55:28 +1300

    added date utility

---
+++ src/utils/date.js
@@ -0,0 +1,9 @@
+/**
+ * @file Provides a `Date` utils
+ * @author Darryl Cousins <darryljcousins@gmail.com>
+ */
+
+import moment from 'moment'
+
+export const STORE_DATE_FORMAT = moment.HTML5_FMT.DATETIMELOCAL
+export const HTML_DATE_FORMAT = "dddd, Do MMMM YYYY"
